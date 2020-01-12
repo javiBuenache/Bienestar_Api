91e30ee436bd466f97d0663b871ae3e49fe74076
Javier <javierbuenache86@gmail.com>
2020-01-12 20:16:42 +0100

    create_middleware

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+use App\Helpers\Token;
+use App\User;
+use Closure;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $header_token = $request->header('Authorization');
+
+        if(isset($header_token))
+        {
+            $token = new Token();
+            $decode_token = $token->decode($header_token);
+            $user= User::where('email', $decode_token)->first();
+
+            if(isset($user))
+            {
+                $request->request->add(['user' => $user]);
+                return $next($request);
+            }
+        }
+        return response()->json(['Message' => 'No tiene permisos'], 401);
+    }
+}
+
